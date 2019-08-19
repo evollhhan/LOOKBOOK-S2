afd544a0bb5fb21110c9437d63d50a910172a489
evollhhan <evollhhan@gmail.com>
2019-08-19 22:17:40 +0800

    update

--- src/component/Show-Board/index.tsx
+++ src/component/Show-Board/index.tsx
@@ -1,7 +1,9 @@
 import React, { memo } from 'react';
 import style from './style.scss';
 import { PROJECTS } from '../../store/use-project';
+import Starter from '../Figures/Starter';
 import MusicAr from '../Figures/Music-Ar';
+
 
 const FIGURES: any[] = [
   MusicAr
@@ -17,6 +19,19 @@ const MainBoard = () => {
   return (
     <div className={style.mainBoard}>
       {/* <span className={style.projectTitle}>HELLO WORLD</span> */}
+      <div className={style.projectItem}>
+        <div className={style.titleArea}>
+          <div className={style.sectionTitle}>PROJECTS</div>
+          <div className={style.subTitle}>项目集</div>
+        </div>
+        <div className={style.projectDesc}>
+          <p>一个好的作品不仅只是网页</p>
+          <p>更是一次有温度的触碰</p>
+          <p>一次眼、手与耳的旅行</p>
+          <p>一种美学的探索</p>
+        </div>
+        <Starter />
+      </div>
       {
         PROJECTS.map((item, index) =>
           <div className={style.projectItem} key={item.tag}>
